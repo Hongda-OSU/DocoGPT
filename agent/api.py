0a3d1deb5e6093adaa79cf9f4fe693e23c501f72
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2024-05-23 15:36:21 -0500

    update backend

--- agent/api.py
+++ agent/api.py
@@ -1,11 +1,14 @@
 import os
 
 from tools import get_graph
-os.environ ["OPENAI_API_KEY"] = "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"
+
+os.environ["OPENAI_API_KEY"] = "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"
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
+
 # from glob import graph, level, pos, embd
 import glob
 import pickle
@@ -13,19 +16,32 @@ from agent.agent_wu import AgentWU
 from parser.pdf_parser import PDFParser
 from config.yaml_config import YAMLConfig
 from vectordb.chroma import Chroma
+from pydantic import BaseModel
+
+class Query(BaseModel):
+    query: str
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 conf = YAMLConfig(file_path="data/search.yaml")
 conf.load()
-search_agent = AgentWU(config=conf).stream(keep = True)
+search_agent = AgentWU(config=conf).stream(keep=True)
+
 
 def set_glob(file: Path):
-    
+
     glob.level = 0
     glob.pos = 0
-    
+
     # level = 0
     # pos = 0
     f = Path(f"data/{file.name}.pkl")
@@ -40,12 +56,14 @@ def set_glob(file: Path):
         glob.graph = parser.from_file(file)
         pickle.dump(glob.graph, open(f"data/{file.name}.pkl", "wb"))
 
+
 @app.post("/uploadfile/")
 def create_upload_file(file: UploadFile):
     with open(f"data/{file.filename}", "wb") as f:
         f.write(file.file.read())
     set_glob(Path(f"data/{file.filename}"))
     return {"filename": file.filename}
+
 
 # @app.post("/uploadfile/")
 # def create_upload_file(file: str):
@@ -54,22 +72,40 @@ def create_upload_file(file: UploadFile):
 
 
 @app.post("/chat/")
-def chat(query: str):
+def chat(query: Query):
     # tmp = get_graph()
     # print(tmp)
     # return {"query": query}
+    query = query.query
     def chat_stream():
-        for i in search_agent.completion(f"{query} The root node is about:\n {glob.graph[-1][0]}"):
-            yield str(dict(type="agent", content=i))
+        for i in search_agent.completion(
+            f"{query} The root node is about:\n {glob.graph[-1][0]}"
+        ):
+            yield dict(type="agent", content=i)
         func, res = search_agent.process()
         if func:
-            yield str(dict(type="func", content=res))
+            yield dict(type="func", content=res)
         while True:
             for i in search_agent.completion(""):
-                yield str(dict(type="agent", content=i))
+                yield dict(type="agent", content=i)
             func, res = search_agent.process()
             if not func:
                 break
-            yield str(dict(type="func", content=res))
+            yield dict(type="func", content=res)
+
     
-    return StreamingResponse(chat_stream())
+    ans = []
+    for i in chat_stream():
+        if len(ans) == 0 or ans[-1][-1]['type'] != i['type']:
+            ans.append([i])
+        else:
+            ans[-1].append(i)
+    ANS = []
+    for i in ans:
+        ANS.append("")
+        for j in i:
+            ANS[-1] += str(j['content'])
+
+    
+
+    return {"data": ANS}
